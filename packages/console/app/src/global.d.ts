695bca728e9850c7b1c61eec3a1823ef4c95df1a
shuv <shuv@shuv.dev>
2026-01-15 21:10:11 -0800

    fix: expand console app webgpu shims

--- packages/console/app/src/global.d.ts
+++ packages/console/app/src/global.d.ts
@@ -3,7 +3,7 @@
 declare module "@stripe/stripe-js"
 declare module "solid-stripe"
 
-export declare module "@solidjs/start/server" {
+declare module "@solidjs/start/server" {
   export type APIEvent = { request: Request }
 }
 
@@ -11,7 +11,7 @@ declare global {
   interface Navigator {
     gpu?: {
       requestAdapter: (...args: unknown[]) => Promise<GPUAdapter | null>
-      getPreferredCanvasFormat?: () => string
+      getPreferredCanvasFormat: () => string
     }
   }
 
@@ -19,12 +19,28 @@ declare global {
     requestDevice: (...args: unknown[]) => Promise<GPUDevice>
   }
 
-  interface GPUDevice {}
+  interface GPUDevice {
+    createShaderModule: (...args: unknown[]) => unknown
+    createBuffer: (...args: unknown[]) => GPUBuffer
+    createBindGroupLayout: (...args: unknown[]) => unknown
+    createBindGroup: (...args: unknown[]) => GPUBindGroup
+    createPipelineLayout: (...args: unknown[]) => unknown
+    createRenderPipeline: (...args: unknown[]) => GPURenderPipeline
+    createCommandEncoder: (...args: unknown[]) => unknown
+    destroy: (...args: unknown[]) => void
+    queue: {
+      writeBuffer: (...args: unknown[]) => void
+      submit: (...args: unknown[]) => void
+    }
+  }
   interface GPUCanvasContext {
-    configure?: (...args: unknown[]) => void
+    configure: (...args: unknown[]) => void
+    getCurrentTexture: (...args: unknown[]) => { createView: (...args: unknown[]) => unknown }
   }
   interface GPURenderPipeline {}
-  interface GPUBuffer {}
+  interface GPUBuffer {
+    destroy: (...args: unknown[]) => void
+  }
   interface GPUBindGroup {}
 
   const GPUBufferUsage: Record<string, number>
