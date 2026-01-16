8bc97ac1629871dce17b0090d66a46f77c298333
shuv <shuv@shuv.dev>
2026-01-15 21:08:13 -0800

    fix: add console app type shims

--- packages/console/app/src/global.d.ts
+++ packages/console/app/src/global.d.ts
@@ -1,5 +1,32 @@
 /// <reference types="@solidjs/start/env" />
 
+declare module "@stripe/stripe-js"
+declare module "solid-stripe"
+
 export declare module "@solidjs/start/server" {
   export type APIEvent = { request: Request }
+}
+
+declare global {
+  interface Navigator {
+    gpu?: {
+      requestAdapter: (...args: unknown[]) => Promise<GPUAdapter | null>
+      getPreferredCanvasFormat?: () => string
+    }
+  }
+
+  interface GPUAdapter {
+    requestDevice: (...args: unknown[]) => Promise<GPUDevice>
+  }
+
+  interface GPUDevice {}
+  interface GPUCanvasContext {
+    configure?: (...args: unknown[]) => void
+  }
+  interface GPURenderPipeline {}
+  interface GPUBuffer {}
+  interface GPUBindGroup {}
+
+  const GPUBufferUsage: Record<string, number>
+  const GPUShaderStage: Record<string, number>
 }
